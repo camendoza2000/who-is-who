a5a4a4a2dfd16cafeb6d684366a51af87d6d252f
Carlos Mendoza <camendoza@sciodev.com>
2014-07-15 16:36:14 -0500

    gatos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140714165558) do
+ActiveRecord::Schema.define(version: 20140715195139) do
+
+  create_table "welcomes", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "wellcomes", force: true do |t|
     t.datetime "created_at"
