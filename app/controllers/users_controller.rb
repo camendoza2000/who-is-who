3254343d990363046addf126aec46acddeb3eab6
Carlos Mendoza <camendoza@sciodev.com>
2014-07-16 17:06:15 -0500

    Create and Read only

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,37 @@
 class UsersController < ApplicationController
   def new
-    
+    @user = User.new
   end
 
   def create
     # render plain: params[:user].inspect
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
 
     @user.save
     redirect_to @user
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.all 
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    
+  end
+
+  
+
+
   private
-    def article_params
-      params.require(:user).permit(:text)
+    def user_params
+      params.require(:user).permit(:name)
     end
 end
