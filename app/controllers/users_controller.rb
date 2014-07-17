abfc1e5341663e806570b4092ccffde769bcf88c
Carlos Mendoza <camendoza@sciodev.com>
2014-07-17 10:51:40 -0500

    full CRUD

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  
   def new
     @user = User.new
   end
@@ -7,8 +9,11 @@ class UsersController < ApplicationController
     # render plain: params[:user].inspect
     @user = User.new(user_params)
 
-    @user.save
-    redirect_to @user
+    if @user.save
+      redirect_to @user
+    else
+      render "new"
+    end
   end
 
   def show
@@ -24,13 +29,25 @@ class UsersController < ApplicationController
   end
 
   def update
-    
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render "edit"
+    end
   end
 
-  
+  def destroy
+    @user.destroy
+
+    redirect_to users_path
+  end
 
 
   private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
     def user_params
       params.require(:user).permit(:name)
     end
