f1968142cf22a7f27880a0e9ed6ccea847ea7368
Carlos Mendoza <camendoza@sciodev.com>
2014-07-21 15:07:04 -0500

    unitary users test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,32 +2,41 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
    
-   test "no name user" do
+   test "no name user" do 
    	user = User.new
    	assert_not user.save, "Saved the user without name"
  	 end
 
-   test "save user" do
+   test "save user" do 
     user = User.new
-    assert user.new_record?, "User not saved"
+    user.name = "cosa"  
+    assert user.save, "User not saved"
    end
 
-   test "delete user" do
-    user = User.new
+   test "delete user" do 
+    count = User.count
+    user = users(:one)
+    user.save
     user.destroy
-    assert user.destroy, "User not deleted"
+    assert count -1 == User.count, "User not deleted" 
    end
 
-   test "delete nonexistent user" do
-   user = user.destroy
-   
+   test "delete nonexistent user" do 
+    ids = []
+    no_user = User.new
+    for user in User.all
+      assert no_user.id != user.id
+    end
    end
 
-   test "update user" do
-   	user = User.new
-   	assert user.update, "User not updated"
+
+     
+    test "update user" do 
+    user = users(:one)
+    user.name = "hi"
+    user.save
+    assert user.name == "hi", "User don't update" 
+
    end
-
-
 
 end
