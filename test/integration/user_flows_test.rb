a63198531264ca6cec005c80ebe57f1ee2736dca
Carlos Mendoza <camendoza@sciodev.com>
2014-07-23 14:49:15 -0500

    integration tests create, edit, delete

--- test/integration/user_flows_test.rb
+++ test/integration/user_flows_test.rb
@@ -10,15 +10,24 @@ class UserFlowsTest < ActionDispatch::IntegrationTest
    	visit "/users/new"
 	  fill_in :user_name, :with => "example" 
   	click_button "Create User"
+    after_create = path
     click_link "Back"
-  	assert counter_user + 1 == User.count
-  end
+    find("tr", text: "example").click_link "Show"
+    assert after_create == path
+    click_link "Back"
+    assert find("tr", text: "example")
+    #assert_redirected_to(controller: "users", action: "show")
+    #expect(current_path).to eq(user)
+    #assert @user_name == "example", "name isn't right"
+
+	end
 
 
   test "show user" do
     visit "/users"
     #click_link "Show"
     first(:link, "Show").click
+    asser path == "/users/"
   end
 
   test "edit user" do
@@ -27,13 +36,17 @@ class UserFlowsTest < ActionDispatch::IntegrationTest
     first(:link, "Edit").click
     fill_in :user_name, :with => "name" 
     click_button "Update User"
+    click_link "Back"
+    assert find("tr", text: "name")
   end
 
   test "delete user" do
     visit "/users"
     #click_link "Delete"
     first(:link, "Delete").click
-  end
+    accept_alert()
+    assert find("tr", text: "MyString")
+   end
 
 
 end
