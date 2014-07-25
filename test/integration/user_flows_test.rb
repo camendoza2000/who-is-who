ffa551d33124e7312739a12eb389a2e64f35da73
Carlos Mendoza <camendoza@sciodev.com>
2014-07-25 14:51:23 -0500

    integration test extended

--- test/integration/user_flows_test.rb
+++ test/integration/user_flows_test.rb
@@ -3,14 +3,21 @@ require "test_helper"
 class UserFlowsTest < ActionDispatch::IntegrationTest
   setup do
     Capybara.current_driver = Capybara.javascript_driver # :seleniun by default
-
   end
 
-def creation()
-   visit "/users/new"
-    fill_in :user_name, :with => "example" 
-    click_button "Create User"
-end
+
+  def creation()
+     visit "/users/new"
+      fill_in :user_name, :with => "example" 
+      click_button "Create User"
+  end
+
+  def edit_user()
+    first(:link, "Edit").click
+    fill_in :user_name, :with => "name" 
+    click_button "Update User"
+    assert page.has_text?("name")
+  end
 
   test "create user" do
     creation()
@@ -27,19 +34,13 @@ end
 
   test "show user" do
     visit "/users"
-    
     first(:link, "Show").click
-    sleep(2)
     assert page.has_text?("name") 
-
   end
 
   test "edit user" do
     visit "/users"
-    first(:link, "Edit").click
-    fill_in :user_name, :with => "name" 
-    click_button "Update User"
-    assert page.has_text?("name")
+    edit_user()
   end
 
   test "delete user" do
@@ -48,6 +49,13 @@ end
     accept_alert()
     assert_not page.has_content?("User1")
    end
+
+  test "prueba" do
+    visit "/users"
+    first(:link, "Show").click
+    edit_user()
+  end
+
 end
 
 
