bfdf9c3b677e9dd1f4683f023995f77d74290e64
Carlos Mendoza <camendoza@sciodev.com>
2014-07-25 11:34:42 -0500

    integration test with a function

--- test/integration/user_flows_test.rb
+++ test/integration/user_flows_test.rb
@@ -6,24 +6,42 @@ class UserFlowsTest < ActionDispatch::IntegrationTest
 
   end
 
+def creation()
+   visit "/users/new"
+    fill_in :user_name, :with => "example" 
+    click_button "Create User"
+end
+
   test "create user" do
-   	visit "/users/new"
-	  fill_in :user_name, :with => "example" 
-  	click_button "Create User"
-    after_create = path 
-    click_link "Back"
+    creation()
+   	#visit "/users/new"
+	  #fill_in :user_name, :with => "example" 
+  	#click_button "Create User"
+    #after_create = path 
+    #click_link "Back"
     #assert_redirected_to(:controller => "users", :action => "show")
-    find("tr", text: "example").click_link "Show"
-    assert after_create == path 
-    click_link "Back"
-    page.assert_selector("tr", text: "example")
+    #find("tr", :text => "example").click_link "Show"
+    #click_link "Back"
+    #assert page.has_text?("waaaaaaaaaaaaa"), "hola"
+    assert page.has_content?("example") 
+    #assert after_create == path 
 	end
+
+  test "check user" do
+    creation()
+    click_link "Back"
+    assert page.has_content?("example") 
+  end
 
 
   test "show user" do
     visit "/users"
+    #show_link = first("a", :text => "Show")
     first(:link, "Show").click
-    assert path == "/users/"
+    #show_link = path 
+    #assert path == show_link
+    #page.assert_selector("p", :text => "Nombre: cosa", :visible => true)
+    assert page.has_text?("My") 
   end
 
   test "edit user" do
@@ -32,7 +50,7 @@ class UserFlowsTest < ActionDispatch::IntegrationTest
     fill_in :user_name, :with => "name" 
     click_button "Update User"
     click_link "Back"
-    assert find("tr", text: "name")
+    assert page.assert_selector("td",:text => "name", :visible => true)
   end
 
   test "delete user" do
@@ -41,6 +59,6 @@ class UserFlowsTest < ActionDispatch::IntegrationTest
     accept_alert()
     assert find("tr", text: "MyString")
    end
-
-
 end
+
+
