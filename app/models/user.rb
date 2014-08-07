4a41155c594e7008c337d6acbb15d99e48e513ac
Carlos Mendoza <camendoza@sciodev.com>
2014-08-07 17:38:38 -0500

    databases relationated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
+	has_many :interests
   validates :name, presence: true
   has_attached_file :avatar, 
-                    :styles => { :medium => "300x300>", :thumb => "100x100>" },
+                    :styles => { :medium => "500x500>", :thumb => "100x100>" },
                     :default_url => "/app/assets/images/:style/missing.png"
   
   validates_attachment_content_type :avatar, 
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,7 @@ Rails.application.routes.draw do
   resources :welcomes
   resources :interests
 
-=begin
-resources :welcomes do
-    resources :users
-  end
-=end
 
-  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
