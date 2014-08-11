647357ea0ee2b48dd1c1314031df824ea974d521
Carlos Mendoza <camendoza@sciodev.com>
2014-08-11 15:37:01 -0500

    new class for avatars

--- app/assets/javascripts/welcomes.js
+++ app/assets/javascripts/welcomes.js
@@ -1,7 +1,7 @@
 
 
 $(document).ready(function(){
-  $("img").click(function(){
+  $(".table_image").click(function(){
     var user_id = this.id
     $("#photo-show").load("/users/"+ user_id);
   });
