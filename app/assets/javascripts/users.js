0e01d1e487285e8a6df2d49b37de4c2a731ea80a
Carlos Mendoza <camendoza@sciodev.com>
2014-08-12 17:33:52 -0500

    The back problem

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,10 @@
+/*
+$(document).ready(function(){
+  console.log("CARGADO");
+  $("#back_link").click(function(){
+    //var user_id = this.id
+  $("#profile-show").load("/welcomes/");
+  });
+}); 
+    
+*/
--- app/assets/javascripts/welcomes.js
+++ app/assets/javascripts/welcomes.js
@@ -1,9 +1,12 @@
 
-
 $(document).ready(function(){
+  console.log("CARGADO");
   $(".table_image").click(function(){
     var user_id = this.id
-    $("#photo-show").load("/users/"+ user_id);
+    //$("#photo-show").load("/users/"+ user_id);
+    $.get("/users/"+ user_id, function(data){
+      $("#photo-show").html(data);
+    });
   });
 }); 
     
