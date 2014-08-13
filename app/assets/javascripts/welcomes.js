cd14adfcbaae6ac5217cfb8c11b167c725fdc8b6
Carlos Mendoza <camendoza@sciodev.com>
2014-08-13 10:05:14 -0500

    back bug fixed

--- app/assets/javascripts/welcomes.js
+++ app/assets/javascripts/welcomes.js
@@ -1,12 +1,8 @@
 
 $(document).ready(function(){
-  console.log("CARGADO");
   $(".table_image").click(function(){
     var user_id = this.id
-    //$("#photo-show").load("/users/"+ user_id);
-    $.get("/users/"+ user_id, function(data){
-      $("#photo-show").html(data);
-    });
+    $("#photo-show").load("/users/"+ user_id);
   });
 }); 
     
