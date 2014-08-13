6c45fedb4a4b2867480823e7006f1f802dee8b8b
Carlos Mendoza <camendoza@sciodev.com>
2014-08-13 14:47:59 -0500

    corrections of ' Interest #21 '

--- app/assets/javascripts/welcomes.js
+++ app/assets/javascripts/welcomes.js
@@ -1,8 +1,8 @@
 
 $(document).ready(function(){
   $(".table_image").click(function(){
-    var user_id = this.id
-    $("#photo-show").load("/users/"+ user_id);
+    var userId = this.id
+    $("#photo-show").load("/users/"+ userId);
   });
 }); 
     
