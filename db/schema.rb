82615fd7e09cea15e746e5235f55c679c5bd7167
Carlos Mendoza <camendoza@sciodev.com>
2014-08-14 12:58:41 -0500

    email is almost ready

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140813223740) do
+ActiveRecord::Schema.define(version: 20140814174008) do
 
   create_table "interests", force: true do |t|
     t.string   "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20140813223740) do
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.text     "responsibilities"
+    t.string   "email"
   end
 
   create_table "welcomes", force: true do |t|
