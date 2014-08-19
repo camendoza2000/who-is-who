dcd386fc7c5e58fcd0181c5900166183fd4cb458
Carlos Mendoza <camendoza@sciodev.com>
2014-08-19 09:49:57 -0500

    funtional tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,17 @@
 Rails.application.routes.draw do
-  resources :users
-
-  #resources :welcomes
-  #resources :interests
-
   resources :users do
     resources :interests
   end
+  resources :welcomes
+  #resources :interests
+
 
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-    root 'welcome#index'
+  #  root 'welcome#index'
 
 
   # Example of regular route:
