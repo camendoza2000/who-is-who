61b32210d3019ad5e59f9c46627cd9ab1ae12384
Carlos Mendoza <camendoza@sciodev.com>
2014-08-20 15:27:43 -0500

    all tests

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,8 +34,5 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
-  # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
-
-  Paperclip.options[:command_path] = "/usr/bin/"
+ 
 end
