529ab95705f6e44e68820172f1f3e86cc8fd08d2
Carlos Mendoza <camendoza@sciodev.com>
2014-08-27 11:03:21 -0500

    corrections 'Capture interests (#26)'

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -1,16 +1,16 @@
 class InterestsController < ApplicationController
 
-	def create
-		@user = User.find(params[:user_id])	
-		@interest = @user.interests.create(interest_params)
-	end
+  def create
+    @user = User.find(params[:user_id]) 
+    @interest = @user.interests.create(interest_params)
+  end
 
-	def index
-	end
+  def index
+  end
 
-	private
-		def interest_params
-			params.require(:interest).permit(:name)
-			
-		end
+  private
+    def interest_params
+      params.require(:interest).permit(:name)
+      
+    end
 end
--- test/integration/welcome_flows_test.rb
+++ test/integration/welcome_flows_test.rb
@@ -5,25 +5,25 @@ class WelcomeFlowsTest < ActionDispatch::IntegrationTest
     Capybara.current_driver = Capybara.javascript_driver # :seleniun by default
   end
 
-  def display_perfil()
+  def display_profile()
     visit("/welcomes/")
     first('img.table_image').click
   end
 
   test "display panel_div" do
-    display_perfil()
+    display_profile()
     assert page.has_selector?("div#profile-avatar")
   end
 
 
   test "display avatar_div" do
-    display_perfil()
+    display_profile()
     assert page.has_selector?("div#profile-avatar")
   end
 
 
   test "back button" do
-    display_perfil()
+    display_profile()
     click_button("Back")
     assert current_path == "/welcomes"
   end
