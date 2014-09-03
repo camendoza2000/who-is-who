fbbb952d27760ec93b64d677c7090bc197b68018
Carlos Mendoza <camendoza@sciodev.com>
2014-09-03 12:22:01 -0500

    expandible section with nice effect

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -19,11 +19,11 @@
 
 $(document).ready(function(){
   $("#expand-button-responsibilities").click(function(){
-     $("div#responsibilities").removeClass( "colapse" );
+     $("div#responsibilities").removeClass( "colapse", 800, "easeOutBounce"  );
   });
 
  $("#expand-button-interests").click(function(){
-     $("div#interests").removeClass( "colapse" );
+     $("div#interests").removeClass( "colapse", 800, "easeOutBounce" );
   });
 
 });
