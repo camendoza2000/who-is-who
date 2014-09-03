48c2bd2c3b72d750f9231aef0aaea0bc6f60a530
Carlos Mendoza <camendoza@sciodev.com>
2014-09-03 17:22:52 -0500

    first new for welcomes

--- test/integration/user_flows_test.rb
+++ test/integration/user_flows_test.rb
@@ -2,8 +2,7 @@ require "test_helper"
 
 class UserFlowsTest < ActionDispatch::IntegrationTest
   setup do
-    #Capybara.current_driver = Capybara.javascript_driver # :seleniun by default
-    Capybara.javascript_driver = :webkit
+    Capybara.current_driver = Capybara.javascript_driver # :seleniun by default
   end
 
 
--- test/integration/welcome_flows_test.rb
+++ test/integration/welcome_flows_test.rb
@@ -2,8 +2,7 @@ require 'test_helper'
 
 class WelcomeFlowsTest < ActionDispatch::IntegrationTest
   setup do
-    #Capybara.current_driver = Capybara.javascript_driver # :seleniun by default
-    Capybara.javascript_driver = :webkit
+    Capybara.current_driver = Capybara.javascript_driver # :seleniun by default
   end
 
   def display_profile()
@@ -29,4 +28,20 @@ class WelcomeFlowsTest < ActionDispatch::IntegrationTest
     assert current_path == "/welcomes"
   end
 
+  test "user show attributes" do
+    display_profile()
+    assert page.has_text?("Thomas Harris")
+    assert assert_text("TM@mail.com")
+    assert page.has_text?("Jackson, Tennessee, U.S.")
+    assert page.has_text?("1940-03-11")
+    assert page.has_text?("Baylor University")
+    assert page.has_text?("English language")
+    assert page.has_content?("web developer")
+    assert page.has_content?("Make the lambs be quiet")
+    assert page.has_content?("dance")
+    image = page.find("img")["src"]
+    assert image.include? "missing.png"
+  end
+
 end
+
